29bcf0fddc7f9ab381748b308bc620d47944e0e9
LUC4Ri0 <varun21999@gmail.com>
2021-06-06 22:24:16 +0530

    successyFully added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,40 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Personal Details</h1>
+
+      <form>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">First Name</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <label for="exampleInputEmail1" className="form-label">Last Name</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">Field 3</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <label for="exampleInputEmail1" className="form-label">Field 4</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        </div>
+        <h1>Account Details</h1>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">Field 5</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <label for="exampleInputEmail1" className="form-label">Field 6</label>
+          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+        </div>
+        
+        <h1>Storage Medium</h1>
+
+        <select class="form-select" aria-label="Default select example">
+          <option value="1">Database</option>
+          <option value="2">Local Storage</option>
+        </select>
+        
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </>
   );
 }
 
